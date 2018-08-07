132aad037f05cfea536e53cc974a5c2e5f79500c
Matthias <m@tth.es>
2018-08-07 20:04:29 +0200

    env

--- flavor/flavor-build.sh
+++ flavor/flavor-build.sh
@@ -13,5 +13,6 @@ apt-get -y install python3 python3-pip
 
 echo 'export PATH="$PATH:/home/user/.local/bin"' >> /home/user/.bashrc
 
-
+echo 'export LC_ALL=C.UTF-8' >> /home/user/.bashrc
+echo 'export LANG=C.UTF-8' >> /home/user/.bashrc
 
